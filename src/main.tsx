75f7c89332e2878348f6f6a59731fb38e09f9012
taillanefer-frx <taillanefer@gmail.com>
2026-07-10 16:29:26 -0300

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from '@/app/App';
+import { registerServiceWorker } from '@/lib/pwa/registerServiceWorker';
+import '@/styles/globals.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+
+registerServiceWorker();
